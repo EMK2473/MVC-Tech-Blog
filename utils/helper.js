6ba9c539f4447fa58bf2a22f2aba690523de8b94
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2023-11-21 16:26:37 -0500

    edit'd helper and dev'd authenticator

--- utils/helper.js
+++ utils/helper.js
@@ -2,13 +2,11 @@ module.exports = {
     format_time: (date) => {
       return date.toLocaleTimeString();
     },
-    // TODO: Create a custom helper 'format_date' that takes in a timestamp,
-    // adds five years to the date, and formats it as M/D/YYYY
     format_date: (timestamp) => {
       const date = new Date(timestamp);
       const month = date.getMonth();
       const day = date.getDate();
-      const year = date.getFullYear() + 5;
-      return `${month + 1}/${day}/${year + 5}`;
+      const year = date.getFullYear();
+      return `${month + 1}/${day}/${year}`;
     },
   };
