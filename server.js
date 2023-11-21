6f0e745b2ecc2936abce9760c6f76bc6950b399f
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2023-11-21 13:29:07 -0500

    init server.js setup

---
+++ server.js
@@ -0,0 +1,39 @@
+//imports
+const express = require("express");
+const session = require("express-session");
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+
+
+// const exphbs = require("express-handlebars");
+// const hbs = exphbs.create({ helpers: require("./utils/helpers") });
+// const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+
+// middleware
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.use(
+  session({
+    secret: process.env.SECRET,
+    store: new SequelizeStore({ db: sequelize }),
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// startServer
+const startServer = async () => {
+    try {
+      await sequelize.sync({ force: false });
+      app.listen(PORT, () => console.log(`Loud and Clear on PORT ${PORT}`));
+    } catch (error) {
+      console.error('ERROR syncing database:', error);
+    }
+  };
+
+  startServer();
