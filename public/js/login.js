a0ce0db7d8e3a5aef89f358ca155fb02ef99822b
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2023-11-24 16:34:25 -0500

    dev'd loginFormHandler

--- public/js/login.js
+++ public/js/login.js
@@ -1,1 +1,26 @@
+const { log } = require("handlebars");
+
 // dev JS functions for logging in
+const loginFormHandler = async (event) =>{
+    event.preventDefault();
+    const username = document.querySelector('#username-login').value.trim();
+    const password =document.querySelector('#password-login').value.trim();
+
+    if(username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' },
+          });
+          if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Failed to log in.')
+    }
+}
+};
+
+const loginForm = document.querySelector('.login-form');
+if (loginForm) {
+    loginForm.addEventListener('submit', loginFormHandler)
+}
