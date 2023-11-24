d4556a0874124fccf8f3675b55c376a58c420898
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2023-11-24 16:17:20 -0500

    dev'd signUpFormHandler and userRoutes

--- controllers/api/userRoute.js
+++ controllers/api/userRoute.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Comment } = require("../../models");
+const { User } = require("../../models");
 
 // get all users
 router.get("/", async (req, res) => {
--- public/js/signUp.js
+++ public/js/signUp.js
@@ -1,1 +1,31 @@
 // dev JS functions for signing up
+const signUpFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username').value.trim();
+    const email = document.querySelector('#email').value.trim();
+    const password = document.querySelector('#password').value.trim();
+  
+    if (username && email && password) {
+      try {
+        const response = await fetch('/api/users/signup', {
+          method: 'POST',
+          body: JSON.stringify({ username, email, password }),
+          headers: { 'Content-Type': 'application/json' },
+        });
+  
+        if (response.ok) {
+          document.location.replace('/'); 
+        } else {
+          alert('Failed to sign up.'); 
+        }
+      } catch (err) {
+        console.error('Error during sign-up:', err);
+        alert('An unexpected error occurred.');
+      }
+    }
+  };
+const signUpForm = document.querySelector('#signup-form');
+if (signUpForm) {
+  signUpForm.addEventListener('submit', signUpFormHandler);
+}
