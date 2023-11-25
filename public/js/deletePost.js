6efbd24f6c3f35c25d697b0ce0fb8002693041af
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2023-11-25 13:24:52 -0500

    dev'd editPost.js

--- public/js/deletePost.js
+++ public/js/deletePost.js
@@ -1,1 +1,20 @@
 // dev JS functions for deleting a post
+const deletePost =  async (event) => {
+    const response = await fetch(`/api/posts/${post.id}`, {
+        method: "DELETE",
+        headers: {"Content-Type": "application/json" },
+    })
+if (response.ok) {
+    document.location.reload()
+} else {
+    alert("Failure deleting post!")
+}
+};
+
+const deletePostHandler = (event) =>{
+    if (event.target.matches('.delete-post')) {
+        const post_id = event.target.getAttribute('data-post-id');
+        deletePost(post_id)
+    }
+}
+document.addEventListener('click', deletePostHandler)
