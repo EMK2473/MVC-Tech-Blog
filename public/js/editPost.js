37db5e55f3f130bc1a85a05562e0611cb8656bef
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2023-11-25 13:34:31 -0500

    dev'd deletePost and editPost js files

--- public/js/editPost.js
+++ public/js/editPost.js
@@ -1,1 +1,47 @@
 // dev JS functions for editing a post
+const post_id = window.location.toString().split('/')[
+    window.location.toString().split("/").length - 1
+]; // get post_id from endpoint by subtracting one from index
+
+// update Post Form Handler
+const updatePostFormHandler = async (event) =>{
+    event.preventDefault();
+    const title = document.querySelector('#title-update-post').value.trim();
+    const content =  document.querySelector('#content-update-post').value.trim();
+    if(title && content) {
+        const response = await fetch(`/api/posts/${post_id}`, {
+            method: "PUT",
+            body: JSON.stringify({ title, content }),
+            headers: { "Content-Type": "application/json" },
+          });
+    if (response.ok) {
+        document.location.replace('/dashboard')
+    }else {
+        alert("Failure to update post!")
+    }
+    }
+};
+
+// delete Post Form Handler
+const deletePostFormHandler = async (event) =>{
+    event.preventDefault();
+    const response = await fetch(`/api/posts/${post_id}`, {
+        method: "DELETE",
+      });
+    if(response.ok){
+        document.location.replace('/dashboard')
+    } else {
+        alert('Failure to delete post!')
+    }
+}
+
+// update post event listener
+const updatePostButton = document.querySelector('#update-post')
+if(updatePostButton){
+    updatePostButton.addEventListener('click', updatePostFormHandler)
+}
+// delete post event listener
+const deletePostButton = document.querySelector('#delete-post');
+if(deletePostButton){
+    deletePostButton.addEventListener('click', deletePostFormHandler)
+}
