856a48e2a7abf55c76630c6564f8d90a5db822e1
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2023-11-25 13:40:59 -0500

    dev'd logout.js and newCOmment.js

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,1 +1,17 @@
 // dev JS functions for logging out
+const logout = async () =>{
+    const response = await fetch('/api/users/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+      });
+    if(response.ok){
+        document.location.replace('/');
+    } else {
+        alert('Faliure to logout!')
+    }
+}
+
+const logoutButton = document.querySelector('#logout');
+if(logout){
+    logoutButton.addEventListener('click', logout)
+}
--- public/js/newComment.js
+++ public/js/newComment.js
@@ -1,1 +1,25 @@
 // dev JS functions for posting a new Comment
+const newCommentFormHandler = async(event) =>{
+    const post_id = parseInt(window.location.pathname.split('/').pop());
+    const content =  document.querySelector('#content-new-comment').ariaValueMax.trim();
+if(content){
+    const response = await fetch(`/api/comments`, {
+        method: 'POST',
+        body: JSON.stringify({ comment_text: content, post_id }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+    if(response.ok){
+        document.location.reload();
+    } else {
+        console.log('Response Status:', response.status);
+        console.log('Response Text:', response.text());
+        alert('Failure to create comment!')
+    }
+}
+}
+
+// event listener
+const newCommentForm = document.querySelector('.new-comment-form');
+if(newCommentForm){
+    newCommentForm.addEventListener('submit', newCommentFormHandler)
+}
